139ff9c123918b38d3d8381762ce197d1cee3fc7
charanpreetkaur026 <charanpreetkaur026@gmail.com>
2020-01-11 10:45:05 -0500

    Initial Commit

---
+++ ImageScrollingWithButtons/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageScrollingWithButtons
+//
+//  Created by charanpreet kaur on 2020-01-11.
+//  Copyright © 2020 charanpreet kaur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
